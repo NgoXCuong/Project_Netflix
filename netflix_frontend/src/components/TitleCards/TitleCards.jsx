8ec44db6779427ace838f605c4d2f2e7f5ace783
NgoXCuong <ngocuongcm2005@gmail.com>
2025-03-02 20:32:31 +0700

    create project

---
+++ netflix_frontend/src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import card_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
+
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MmZjODIwNWYyMzc0OTNhZTVlZDY3NjcyMzFhZDhjMCIsIm5iZiI6MTc0MDgzNzIzOC40Miwic3ViIjoiNjdjMzExNzY1ODU1OTE0NTIzM2Y2YmMyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.63iL0kg4LF8kj8meUUpRo1E14SsOj4K7usQdypYpzFM",
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
+    const cardsContainer = cardsRef.current;
+    if (!cardsContainer) return;
+
+    cardsContainer.addEventListener("wheel", handleWheel);
+
+    // Cleanup khi component unmount
+    return () => {
+      cardsContainer.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
