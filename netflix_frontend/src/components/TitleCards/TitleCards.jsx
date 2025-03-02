a41471417d57c84c10543bb30582d6f9a5279b77
NgoXCuong <ngocuongcm2005@gmail.com>
2025-03-02 22:12:21 +0700

    second

--- netflix_frontend/src/components/TitleCards/TitleCards.jsx
+++ netflix_frontend/src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import card_data from "../../assets/cards/Cards_data";
+// import card_data from "../../assets/cards/Cards_data";
 import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
---
+++ netflix_frontend/src/firebase.js
@@ -0,0 +1,59 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkiJcYNwHHxM525TiDWaLh-H__Hd7s7_0",
+  authDomain: "project-netflix-3aadb.firebaseapp.com",
+  projectId: "project-netflix-3aadb",
+  storageBucket: "project-netflix-3aadb.firebasestorage.app",
+  messagingSenderId: "788809817574",
+  appId: "1:788809817574:web:a81e1a6ce98ce7607b0a27",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.error(error);
+    alert(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const login = async () => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, login, signup, logout };
